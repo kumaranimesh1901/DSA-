3737d2e2898ad185283da8b60eb20fba6dcc8656
kumaranimesh1901 <kumaranimesh1901@gmail.com>
2026-04-25 00:54:06 +0530

    bfs graph

---
+++ dsa.cpp/BFS.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<list>
+
+using namespace std;
+class graph
+{
+    int v;
+    list<int>*l;
+    public:
+    graph(int v)
+    {
+        this->v = v;
+        l = new list<int>[v];
+    }
+    void addEdge(int u, int v)
+    {
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    void bfs()
+    {
+        queue<int> Q;
+        vector<bool> visisted(v,false);
+        Q.push(0);
+        visisted[0]=true;
+        while(Q.size()>0)
+        {
+            int u=Q.front();
+            Q.pop();
+            cout<<u;
+            for(auto v:l[u])
+            {
+                if(!visisted[v])
+                {
+                    visisted[v]=true;
+                    Q.push(v);
+                }
+                
+            }
+        }
+    }
+};
+int main()
+{
+    graph g(6);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    g.addEdge(3,5);
+    g.bfs();
+}   
