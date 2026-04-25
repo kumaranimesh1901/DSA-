667671c1d71eb9a0fc07f795e72ac199e383ae6e
kumaranimesh1901 <kumaranimesh1901@gmail.com>
2026-04-26 00:27:10 +0530

    dfs

--- dsa.cpp/BFS.cpp
+++ dsa.cpp/BFS.cpp
@@ -41,14 +41,34 @@ class graph
             }
         }
     }
+    void dfshelper(int u, vector<bool> &visited)
+    {
+        cout<<u;
+        visited[u]=true;
+        for(auto v:l[u])
+        {
+            if(!visited[v])
+            {
+                dfshelper(v,visited);
+            }
+        }
+    }
+    void dfs()
+    {
+        vector<bool> visited(v,false);
+        dfshelper(0,visited);
+    }
 };
 int main()
 {
     graph g(6);
     g.addEdge(0,1);
-    g.addEdge(0,2);
+    g.addEdge(1,2);
     g.addEdge(1,3);
+    
     g.addEdge(2,4);
-    g.addEdge(3,5);
     g.bfs();
+    cout<<endl;
+    g.dfs();
+
 }   
